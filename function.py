7bf4ef96a8c69c42b29e69963cbeddd3de5b7c15
Jérémy Bochard <jeremy.bochard@gmail.com>
2021-02-21 16:45:05 +0100

    Add description for function & rewrite get_url_books_for_a_category_page function

--- function.py
+++ function.py
@@ -7,6 +7,14 @@ from bs4 import BeautifulSoup as bs
 
 def get_book_informations(url):
 
+    """
+    Scrap an url of a book
+    Args:
+        url (str): The book's url scrapped
+    Returns:
+        data: A dictionnary wich contains all the data scrapped
+    """
+
     data = {}
 
     response = requests.get(url)
@@ -46,7 +54,15 @@ def get_book_informations(url):
     return data
 
 
-def write_csv(book_list, csv_name):
+def write_csv(books_list, csv_name):
+
+    """
+    Write a csv file of all the books' informations wich are in the books_list
+    Args:
+        books_list (list): list of the dictionaries wich contain the informations we want to write in a csv file
+        csv_name (str): name of the csv file we want to write
+    """
+
     with open(csv_name, "w") as p:
         fnames = [
             "product_page_url",
@@ -62,92 +78,119 @@ def write_csv(book_list, csv_name):
         ]
         csv_writer = csv.DictWriter(p, delimiter="|", fieldnames=fnames)
         csv_writer.writeheader()
-        if book_list:
-            for book in book_list:
+        if books_list:
+            for book in books_list:
                 csv_writer.writerow(book)
 
 
 def download_image(book):
+    """
+    Download image of a book
+    Args:
+        book (dict): dictionary wich represent the book that we want to download image
+    """
+
     response = requests.get(book["image_url"])
-    with open(book["title"] + ".jpg", "wb") as book:
-        book.write(response.content)
-        book.close()
+    with open(
+        "".join(e for e in book["title"] if e.isalnum()) + ".jpg", "wb"
+    ) as book_image:
+        book_image.write(response.content)
+        book_image.close()
 
 
 def get_url_books_for_a_category_page(url):
+    """
+    Make a list of the book's url from a category's page
+    Manage categories' pagination
+    Args:
+        url (str): The category's url scrapped
+    Returns:
+        book's_links: A list wich contains all book's url from a category's page
+    """
+
+    category_urls = []
     books_links = []
     response = requests.get(url)
     if response.ok:
         soup = bs(response.text, "lxml")
         category_pagination = soup.find("li", {"class": "current"})
-        if category_pagination is None:
-            book_urls = soup.find_all(
-                "li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"}
-            )
-            for book_url in book_urls:
-                a_book = book_url.find("a")
-                books_links.append(
-                    a_book["href"].replace(
-                        "../../..", "http://books.toscrape.com/catalogue"
-                    )
-                )
-        else:
+        if category_pagination:
+
             num_pagination = int(
                 category_pagination.text.strip().replace("Page 1 of ", "")
             )
-            category_urls = []
+
             j = 1
             for i in range(num_pagination):
                 category_urls.append(
                     url.replace("index.html", "page-") + str(j) + ".html"
                 )
                 j += 1
-            for category_url in category_urls:
-                response = requests.get(category_url)
-                if response.ok:
-                    soup = bs(response.text, "lxml")
-                    book_urls = soup.find_all(
-                        "li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"}
-                    )
-                    for book_url in book_urls:
-                        a_book = book_url.find("a")
-                        books_links.append(
-                            a_book["href"].replace(
-                                "../../..", "http://books.toscrape.com/catalogue"
-                            )
+
+        else:
+            category_urls.append(url)
+
+        for category_url in category_urls:
+            response = requests.get(category_url)
+            if response.ok:
+                books_informations = soup.find_all(
+                    "li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"}
+                )
+
+                for book_informations in books_informations:
+                    a_book = book_informations.find("a")
+                    books_links.append(
+                        a_book["href"].replace(
+                            "../../..", "http://books.toscrape.com/catalogue"
                         )
+                    )
     return books_links
 
 
 def get_all_url_category():
-
-    category_links = []
+    """
+    Make a list of the categories's url from the site
+    Returns:
+        categorie's_links: A list wich contains all categorie's url from the site
+    """
+    categories_links = []
     url = "http://books.toscrape.com/"
     response = requests.get(url)
     if response.ok:
         soup = bs(response.text, "lxml")
-        category_urls = (
+        raw_categories_links = (
             soup.find("ul", {"class": "nav nav-list"}).find("li").find_all("li")
         )
-        for category_url in category_urls:
-            a_category = category_url.find("a")
-            category_links.append("http://books.toscrape.com/" + a_category["href"])
-    return category_links
+        for raw_category_link in raw_categories_links:
+            a_category = raw_category_link.find("a")
+            categories_links.append("http://books.toscrape.com/" + a_category["href"])
+    return categories_links
 
 
-def get_books_category_name(url_books_category):
+def get_books_category_name(url):
+    """
+    Get the category's name from a category's url
+    Args:
+        url (str): The category's url that the name is got
+    Returns:
+        books_category_name: The name of book's category
+    """
+
     books_category_name = (
-        url_books_category.replace(
-            "http://books.toscrape.com/catalogue/category/books/", ""
-        )
+        url.replace("http://books.toscrape.com/catalogue/category/books/", "")
         .replace("/index.html", "")
         .split("_")
     )
-    books_category_name = books_category_name[0].replace("-", " ").capitalize()
+    books_category_name = books_category_name[0].replace("-", "_").capitalize()
     return books_category_name
 
 
 def print_time(raw_interval):
+    """
+    Calculate and print the time in minutes and seconds from a time in seconds
+    Args:
+        raw_interval (int): A time in seconds
+    """
     interval_in_min = raw_interval / 60
     extra_sec = round(interval_in_min % 1, 2)
     interval_in_sec = round(extra_sec * 60)
--- scraper.py
+++ scraper.py
@@ -8,13 +8,13 @@ choice = input("Scrape [book] [category] [all] page : ").lower()
 if choice == "book":
     book = []
     data = function.get_book_informations(
-        "http://books.toscrape.com/catalogue/eat-fat-get-thin_688/index.html"
+        "http://books.toscrape.com/catalogue/red-hoodarsenal-vol-1-open-for-business-red-hoodarsenal-1_729/index.html"
     )
     book.append(data)
     function.write_csv(book, "book_information.csv")
     function.download_image(data)
 
-if choice == "category":
+if choice == "c":
 
     book_links = function.get_url_books_for_a_category_page(
         "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
@@ -32,16 +32,18 @@ if choice == "category":
 if choice == "all":
     start_time = time.time()
     k = 0
+    books = []
     category_links = function.get_all_url_category()
     for category_link in category_links:
         books_category_name = function.get_books_category_name(category_link)
         book_links = function.get_url_books_for_a_category_page(category_link)
-        books = []
         for book_link in book_links:
             data = function.get_book_informations(book_link)
             function.download_image(data)
             books.append(data)
-        function.write_csv(books, books_category_name.replace(" ", "_") + ".csv")
-        print("File " + books_category_name.replace(" ", "_") + ".csv has been updated")
+            k += 1
+            print(k)
+        function.write_csv(books, books_category_name + ".csv")
+        print("File " + books_category_name + ".csv has been updated")
     interval = time.time() - start_time
     function.print_time(interval)
